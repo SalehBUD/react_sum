1e7be5c305f69ca0022edc2b44d543504ceca9d7
Saleh BUD <saleh.7734@gmail.com>
2023-01-12 11:30:09 +0200

    Complete

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 export default function Sum({ a = 0, b = 0 }) {
   return (
     <p>
-      Sum of {a} and {b} is {a + b}
+      Sum of
+      {' '}
+      {a}
+      {' '}
+      and
+      {' '}
+      {b}
+      {' '}
+      is
+      {' '}
+      {a + b}
     </p>
   );
 }
